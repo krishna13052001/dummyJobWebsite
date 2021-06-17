7e93dc7cbd5de181b1d5ba76c4e7ba526e9a28a0
Krishna <sathya13052001krishna@gmail.com>
2021-06-17 14:48:24 +0530

    First commit

---
+++ protfolio/jobs/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    companyName = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=500)
